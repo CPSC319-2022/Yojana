dbd40264ca28dbd1d3c47faf130f0e81f2e83bb7
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-01-27 02:37:31 -0800

    Added /api/users route

---
+++ src/pages/api/users/[id].ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '@/lib/prismadb'
+import { getToken } from 'next-auth/jwt'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const token = await getToken({ req })
+  const { id } = req.query
+
+  if (!token?.isAdmin && token?.id !== id) {
+    res.status(401).send('Unauthorized')
+  } else {
+    const { id } = req.query
+    if (!id) return res.status(400).send('Bad Request')
+    if (typeof id !== 'string') return res.status(400).send('Bad Request')
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: id
+      }
+    })
+
+    if (!user) return res.status(404).send('Not Found')
+    return res.status(200).json(user)
+  }
+}
+
+export default handler
---
+++ src/pages/api/users/index.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getToken } from 'next-auth/jwt'
+import prisma from '@/lib/prismadb'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const token = await getToken({ req })
+  if (!token?.isAdmin) {
+    res.status(401).send('Unauthorized')
+  } else {
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        isAdmin: true
+      }
+    })
+    res.status(200).json(users)
+  }
+}
+
+export default handler
