2fd367cd7ba548b3c8347e398c282a7e6107eddc
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-01-27 02:38:54 -0800

    Renamed file and relocated all Categories route to /api/cats

--- src/pages/api/cats/index.ts
+++ src/pages/api/cats/index.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { Prisma } from '@prisma/client'
 import prisma from '@/lib/prismadb'
 
-type Category = Prisma.CategoryGetPayload<{
+type Categories = Prisma.CategoryGetPayload<{
   select: {
     id: true
     name: true
@@ -10,11 +10,10 @@ type Category = Prisma.CategoryGetPayload<{
     color: true
     isMaster: true
     creator: true
-    dates: true
   }
 }>[]
 
-const handler = async (req: NextApiRequest, res: NextApiResponse<Category>) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse<Categories>) => {
   const categories = await prisma.category.findMany({
     select: {
       id: true,
@@ -22,8 +21,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<Category>) => {
       description: true,
       color: true,
       isMaster: true,
-      creator: true,
-      dates: true
+      creator: true
     }
   })
   res.status(200).json(categories)
