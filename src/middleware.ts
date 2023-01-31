dc6591075c191d2780a34666f5533721a7599503
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-01-30 17:57:11 -0800

    Protect all api routes with use authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,4 +2,4 @@ export { default } from 'next-auth/middleware'
 
 // matcher contains page routes that should be protected by authentication middleware
 // https://next-auth.js.org/configuration/nextjs#middleware
-export const config = { matcher: ['/'] }
+export const config = { matcher: ['/', '/api/:path*'] }
