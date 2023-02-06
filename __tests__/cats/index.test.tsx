6878522861761854f5bd969afac7329dd7079244
Rithin Kumar <rithinkumar13@gmail.com>
2023-02-05 16:32:08 -0800

    DELETE cats implemented, and related tests

--- __tests__/cats/index.test.tsx
+++ __tests__/cats/index.test.tsx
@@ -2,9 +2,24 @@ import { prismaMock } from '@/lib/singleton'
 import cats from '@/pages/api/cats'
 import '@testing-library/jest-dom'
 import { createRequest, createResponse } from 'node-mocks-http'
+import * as jwt from "next-auth/jwt";
 
 const generateISODates = () => {
   return Array.from({ length: 5 }, (_, i) => new Date(`2023-01-0${i + 1}`).toISOString())
+}
+
+const mockAdmin = {
+  id: '1',
+  name: 'John Doe',
+  email: 'john.doe@ad.com',
+  isAdmin: true
+}
+
+const mockUser = {
+  id: '1',
+  name: 'John Doe',
+  email: 'john.doe@ad.com',
+  isAdmin: false
 }
 
 describe('/api/cats', () => {
@@ -99,6 +114,114 @@ describe('/api/cats', () => {
 
       expect(res._getStatusCode()).toBe(409)
       expect(res._getData()).toBe('category name must be unique')
+    })
+  })
+
+  describe('DELETE', () => {
+    it('should return a 200 status code when category is deleted', async () => {
+      const mock_body = {
+        id: 0,
+        name: 'category to delete',
+        description: 'new desc',
+        color: '#000000',
+        isMaster: false,
+        creatorId: 'authorized user'
+      }
+      const req = createRequest({
+        method: 'DELETE',
+        url: '/cats',
+        body: mock_body
+      })
+      const res = createResponse()
+
+      jest.spyOn(jwt, 'getToken').mockResolvedValue(mockAdmin)
+
+      prismaMock.category.findFirst.mockResolvedValue(mock_body)
+
+      prismaMock.category.delete.mockResolvedValue(mock_body)
+
+      await cats(req, res)
+
+      expect(res._getStatusCode()).toBe(200)
+      expect(res._getData()).toBe(JSON.stringify(mock_body))
+    })
+
+    it('should return a 401 status code when not Authorized to delete', async () => {
+      const mock_body = {
+        id: 0,
+        name: 'category to delete',
+        description: 'new desc',
+        color: '#000000',
+        isMaster: false,
+        creatorId: 'unauthorized user'
+      }
+      const req = createRequest({
+        method: 'DELETE',
+        url: '/cats',
+        body: mock_body
+      })
+      const res = createResponse()
+
+      jest.spyOn(jwt, 'getToken').mockResolvedValue(mockUser)
+
+      await cats(req, res)
+
+      expect(res._getStatusCode()).toBe(401)
+      expect(res._getData()).toBe("Unauthorized")
+    })
+
+    it('should return a 404 status code when category does not exist', async () => {
+      const mock_body = {
+        id: 999999,
+        name: 'not_existing',
+        description: 'abc',
+        color: '#000000',
+        isMaster: false,
+        creatorId: '1'
+      }
+      const req = createRequest({
+        method: 'DELETE',
+        url: '/cats',
+        body: mock_body
+      })
+      const res = createResponse()
+
+      jest.spyOn(jwt, 'getToken').mockResolvedValue(mockAdmin)
+
+      prismaMock.category.update.mockRejectedValue(new Error('category does not exist'))
+
+      await cats(req, res)
+
+      expect(res._getStatusCode()).toBe(404)
+      expect(res._getData()).toBe('category does not exist')
+    })
+
+    it('should return a 409 when there is an error processing request', async () => {
+      const mock_body = {
+        id: 999999,
+        name: 'not_existing',
+        description: 'abc',
+        color: '#000000',
+        isMaster: false,
+        creatorId: '1'
+      }
+      const req = createRequest({
+        method: 'DELETE',
+        url: '/cats',
+        body: mock_body
+      })
+      const res = createResponse()
+
+      jest.spyOn(jwt, 'getToken').mockResolvedValue(mockAdmin)
+
+      prismaMock.category.findFirst.mockResolvedValue(mock_body)
+
+      prismaMock.category.delete.mockRejectedValue(new Error('processing error'))
+
+      await cats(req, res)
+
+      expect(res._getStatusCode()).toBe(409)
+      expect(res._getData()).toBe('There was an error deleting the category')
     })
   })
 
