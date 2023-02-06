cc4c559661a6f56119598473b24478a40ada33ff
dashdot <30674558+Graywing13@users.noreply.github.com>
2023-02-05 18:19:46 -0800

    Add file to specify tailwind dynamic variables.

---
+++ src/constants/tailwindDynamicVars.ts
@@ -0,0 +1,16 @@
+// Tailwind dynamic variables have to be specified in a comment to be registered.
+// Otherwise, the styles will not show up at runtime.
+
+/**
+ * Sizes
+ * h-1/6
+ * h-1/5
+ */
+
+/**
+ * Colors
+ * Pink: bg-[#F04DEA]
+ * Green: bg-[#4DF07B]
+ */
+
+export {}
