7ae65399f1dd1587d683b3d64a6c0cf2b5b561ca
AnimeAllstar <AnimeAllstar@users.noreply.github.com>
2023-02-07 00:30:30 +0000

    Format code

--- src/components/categoriesMenu/CategoriesMenu.tsx
+++ src/components/categoriesMenu/CategoriesMenu.tsx
@@ -1,5 +1,5 @@
-import {Checkbox} from '@/components/common'
-import React, {ReactElement, useEffect, useMemo, useState } from 'react'
+import { Checkbox } from '@/components/common'
+import React, { ReactElement, useEffect, useMemo, useState } from 'react'
 import { Category, EventWatcher } from '@/utils/calendar'
 import { BsPencilSquare } from 'react-icons/bs'
 
@@ -13,9 +13,9 @@ export const CategoriesMenu = (): ReactElement => {
 
   const eventList = useMemo(() => {
     return events.map((calEvent, key) => (
-      <div className={`flex-row flex justify-between bg-[#${calEvent.color}] mt-1`} key={`category-item-${key}`}>
+      <div className={`flex flex-row justify-between bg-[#${calEvent.color}] mt-1`} key={`category-item-${key}`}>
         <Checkbox color={calEvent.color} label={calEvent.categoryName} id={`checkbox-${key}`} key={`checkbox-${key}`} />
-      <span className="mr-2 cursor-pointer">
+        <span className='mr-2 cursor-pointer'>
           <BsPencilSquare />
         </span>
       </div>
