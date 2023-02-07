856085468a91942cb3a94902b7485a4124e15592
Asad Dhorajiwala <asadrehandhorajiwala@gmail.com>
2023-02-06 16:41:50 -0800

    Bad formatting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,12 @@ import { Provider } from 'react-redux'
 import { SessionProvider } from 'next-auth/react'
 import { AppProps } from 'next/app'
 
-const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+const App = ({Component, pageProps: { session, ...pageProps }}: AppProps) => {
   return (
     <SessionProvider session={session}>
       <Head>
         <title>Yojana</title>
-        <meta name='description' content='Yojana is a calendar app built with Next.js' />
+      <meta name="description" content='Yojana is a calendar app built with Next.js' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
