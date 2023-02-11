3d3a40ab6f888733e3f44711b956eb9f833ccdbc
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-02-11 05:31:57 -0800

    Only show hover effect when button is enabled

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -9,7 +9,7 @@ export const Button = ({ text, onClick, type, disabled, className }: ButtonProps
     <button
       type={type}
       onClick={onClick}
-      className={`inline-flex justify-center rounded-md border border-transparent bg-emerald-100 px-4 py-2 text-sm font-medium text-emerald-900 hover:bg-emerald-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-2 disabled:opacity-50 ${className}`}
+      className={`inline-flex justify-center rounded-md border border-transparent bg-emerald-100 px-4 py-2 text-sm font-medium text-emerald-900 focus:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-2 enabled:hover:bg-emerald-200 disabled:opacity-75 ${className}`}
       disabled={disabled}
     >
       {text}
