dcfefc9c014318b01bf4a75f9f334cd929a373f6
Lymeng Naret <lymengnaret@yahoo.com>
2023-02-12 01:24:04 -0800

    hide horizontal overflow for category names

--- src/components/mainCalendar/EventBlock.tsx
+++ src/components/mainCalendar/EventBlock.tsx
@@ -11,13 +11,14 @@ interface EventBlockProps {
 
 export const EventBlock = (props: EventBlockProps): ReactElement => {
   const monthView = useAppSelector(isMonthInterval)
-  const label = props.label.length > 18 ? props.label.substring(0, 18) + '...' : props.label
   return (
     <div
       aria-label={props.label}
-      className={`bg-[${props.color}] mt-1 ${getTextColor(props.color)} max-h-[2.5vh] min-h-[1vh] pl-1 pr-1`}
+      className={`bg-[${props.color}] mt-1 ${getTextColor(
+        props.color
+      )} min-h-[1vh] overflow-x-hidden whitespace-nowrap pl-1 pr-1`}
     >
-      {monthView ? props.icon + ' ' + label : ''}
+      {monthView ? props.icon + ' ' + props.label : ''}
     </div>
   )
 }
