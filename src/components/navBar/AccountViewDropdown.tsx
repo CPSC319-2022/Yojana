d5c304f47565a7d22311a5212534f219dd8129fb
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-02-13 05:28:36 -0800

    Added Dropdown common component

--- src/components/navBar/AccountViewDropdown.tsx
+++ src/components/navBar/AccountViewDropdown.tsx
@@ -1,66 +1,27 @@
-import React, { Fragment, ReactElement } from 'react'
+import React, { ReactElement } from 'react'
 import { signOut, useSession } from 'next-auth/react'
-import { Menu, Transition } from '@headlessui/react'
+import { Dropdown } from '@/components/common'
 
 export const AccountViewDropdown = (): ReactElement => {
   const { data: session } = useSession()
   const sessionU = session?.user.isAdmin ? 'Admin' : 'User'
 
-  //  const renderItems = useMemo(() => {
-  const onSelect = (selectedKey: string) => {
-    if (selectedKey == 'Logout' && selectedKey !== null) {
-      signOut().then(() => {
-        console.log('Logged out')
-      })
-    } else {
-    }
-  }
-
   return (
-    <div id='account-view-menu' className='dropdown' title='account'>
-      <Menu as='div' className='relative inline-block text-left'>
-        <div>
-          <Menu.Button className='inline-flex w-full justify-center rounded-md border border-gray-200 bg-white bg-opacity-20 px-4 py-2 font-medium hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75'>
-            Account
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter='transition ease-out duration-100'
-          enterFrom='transform opacity-0 scale-95'
-          enterTo='transform opacity-100 scale-100'
-          leave='transition ease-in duration-75'
-          leaveFrom='transform opacity-100 scale-100'
-          leaveTo='transform opacity-0 scale-95'
-        >
-          <Menu.Items className='absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-            <div className='px-1 py-1 '>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    key={'Logout'}
-                    onClick={() => onSelect('Logout')}
-                    className={`${active ? 'bg-gray-300' : ''} group flex w-full items-center rounded-md px-2 py-2`}
-                  >
-                    Logout
-                  </button>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    key={'ID'}
-                    onClick={() => onSelect('ID')}
-                    className={`${active ? 'bg-gray-300' : ''} group flex w-full items-center rounded-md px-2 py-2`}
-                  >
-                    {sessionU}
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
-    </div>
+    <Dropdown
+      title='Account'
+      containerClassName='w-[12vw]'
+      menuItems={[
+        {
+          key: 'Logout',
+          label: 'Logout',
+          onClick: () => signOut()
+        },
+        {
+          key: 'User',
+          label: sessionU,
+          onClick: () => {}
+        }
+      ]}
+    />
   )
 }
--- src/components/navBar/CalViewDropdown.tsx
+++ src/components/navBar/CalViewDropdown.tsx
@@ -1,75 +1,34 @@
-import React, { Fragment, ReactElement } from 'react'
+import React, { ReactElement } from 'react'
 import { getInterval, setInterval } from '@/redux/reducers/MainCalendarReducer'
 import { CalendarInterval } from '@/constants/enums'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
-import { Menu, Transition } from '@headlessui/react'
+import { Dropdown } from '@/components/common'
 
 export const CalViewDropdown = (): ReactElement => {
   const dispatch = useAppDispatch()
   const activeCalView = useAppSelector(getInterval)
 
-  const onSelect = (selectedKey: string) => {
-    if (selectedKey !== activeCalView && selectedKey !== null) {
-      dispatch(setInterval(selectedKey as CalendarInterval))
-    }
-  }
-
   return (
-    <div id='calendar-view-menu' className='dropdown' title={activeCalView}>
-      <Menu as='div' className='relative inline-block text-left'>
-        <div>
-          <Menu.Button className='inline-flex w-full justify-center rounded-md border border-gray-200 bg-white bg-opacity-20 px-4 py-2 font-medium hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75'>
-            {activeCalView}
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter='transition ease-out duration-100'
-          enterFrom='transform opacity-0 scale-95'
-          enterTo='transform opacity-100 scale-100'
-          leave='transition ease-in duration-75'
-          leaveFrom='transform opacity-100 scale-100'
-          leaveTo='transform opacity-0 scale-95'
-        >
-          <Menu.Items className='absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-            <div className='px-1 py-1 '>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    key={'Year'}
-                    onClick={() => onSelect('Year')}
-                    className={`${active ? 'bg-gray-300' : ''} group flex w-full items-center rounded-md px-2 py-2`}
-                  >
-                    Year
-                  </button>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    key={'4 Months'}
-                    onClick={() => onSelect('4 Months')}
-                    className={`${active ? 'bg-gray-300' : ''} group flex w-full items-center rounded-md px-2 py-2`}
-                  >
-                    4 Months
-                  </button>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    key={'Month'}
-                    onClick={() => onSelect('Month')}
-                    className={`${active ? 'bg-gray-300' : ''} group flex w-full items-center rounded-md px-2 py-2`}
-                  >
-                    Month
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
-    </div>
+    <Dropdown
+      title={activeCalView}
+      containerClassName='w-[12vw]'
+      menuItems={[
+        {
+          key: CalendarInterval.YEAR,
+          label: CalendarInterval.YEAR,
+          onClick: () => dispatch(setInterval(CalendarInterval.YEAR))
+        },
+        {
+          key: CalendarInterval.FOUR_MONTHS,
+          label: CalendarInterval.FOUR_MONTHS,
+          onClick: () => dispatch(setInterval(CalendarInterval.FOUR_MONTHS))
+        },
+        {
+          key: CalendarInterval.MONTH,
+          label: CalendarInterval.MONTH,
+          onClick: () => dispatch(setInterval(CalendarInterval.MONTH))
+        }
+      ]}
+    />
   )
 }
