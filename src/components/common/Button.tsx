62952213e9dbc00855d2c2f8efecbd342c6e666e
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-02-13 06:20:14 -0800

    Fixed bug
    https://github.com/tailwindlabs/headlessui/issues/856

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text: string
 }
 
-export const Button = ({ text, onClick, type, disabled, className }: ButtonProps) => {
-  return (
-    <button
-      type={type}
-      onClick={onClick}
-      className={`inline-flex justify-center rounded-md border border-transparent bg-emerald-100 px-4 py-2 font-medium text-emerald-900 focus:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-2 enabled:hover:bg-emerald-200 disabled:opacity-75 ${className}`}
-      disabled={disabled}
-    >
-      {text}
-    </button>
-  )
-}
+export const Button = forwardRef(
+  ({ text, onClick, type, disabled, className }: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
+    return (
+      <button
+        type={type}
+        onClick={onClick}
+        className={`inline-flex justify-center rounded-md border border-transparent bg-emerald-100 px-4 py-2 font-medium text-emerald-900 focus:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-2 enabled:hover:bg-emerald-200 disabled:opacity-75 ${className}`}
+        disabled={disabled}
+        ref={ref}
+      >
+        {text}
+      </button>
+    )
+  }
+)
+
+Button.displayName = 'Button'
