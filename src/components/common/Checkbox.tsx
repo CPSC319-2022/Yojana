f72e52b391d9b32aa7f582fb484fd038776eda30
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-02-13 06:43:39 -0800

    Fixed checkbox accent color issue using inline style

--- src/components/common/Checkbox.tsx
+++ src/components/common/Checkbox.tsx
@@ -9,26 +9,13 @@ interface CheckboxProps {
 }
 
 export const Checkbox = ({ color, id, checkboxClassName, label, wrapperClassName }: CheckboxProps): ReactElement => {
-  if (color === '#f59e0b') {
-    color = 'accent-orange-400'
-  } else if (color === '#10b981') {
-    color = 'accent-green-400'
-  } else if (color === '#0ea5e9') {
-    color = 'accent-blue-400'
-  } else if (color === '#ef4444') {
-    color = 'accent-red-400'
-  } else if (color === '#0f172a') {
-    color = 'accent-gray-400'
-  } else if (color === '#8b5cf6') {
-    color = 'accent-purple-400'
-  }
-
   return (
     <div className={wrapperClassName}>
       <input
         type='checkbox'
         id={id}
         className={`${color} ${checkboxClassName} relative bottom-px align-middle`}
+        style={{ accentColor: color }}
         defaultChecked
       />
       <label className='ml-2' htmlFor={id}>
