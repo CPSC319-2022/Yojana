b5511f4ac3956264e5870a5714dcdbae68eb9f37
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-02-12 21:15:35 -0800

    Updated nav buttons

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -2,8 +2,8 @@ import React, { ReactElement } from 'react'
 import { AccountViewDropdown } from './AccountViewDropdown'
 import { CalViewDropdown } from './CalViewDropdown'
 import { decrementDate, getDate, incrementDate, isYearInterval } from '@/redux/reducers/MainCalendarReducer'
-import { signOut, useSession } from 'next-auth/react'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import { Button } from '@/components/common'
 
 interface NavBarProps {
   className: string
@@ -13,25 +13,14 @@ export const NavBar = (props: NavBarProps): ReactElement => {
   const dispatch = useAppDispatch()
   const targetDate = useAppSelector(getDate)
   const yearView = useAppSelector(isYearInterval)
-  const { data: session } = useSession()
 
   return (
     <div className={props.className + ' ' + 'justify-between'}>
-      <h1 className='text-2xl font-medium'>Calendar</h1>
+      <h1 className='text-2xl font-medium'>Yojana</h1>
       <div className='flex flex-row'>
-        <button
-          className='btn border border-gray-200 bg-white px-2 py-1 text-sm font-medium text-black focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-600 focus-visible:ring-offset-2 enabled:hover:bg-gray-100 disabled:opacity-75 '
-          onClick={() => dispatch(decrementDate())}
-        >
-          &lt;
-        </button>
-        <h4 className='ml-3 mr-3 text-center text-xl'>{targetDate.format(yearView ? 'YYYY' : 'MMMM YYYY')}</h4>
-        <button
-          className='btn border border-gray-200 bg-white px-2 py-1 text-sm font-medium text-black focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-600 focus-visible:ring-offset-2 enabled:hover:bg-gray-100 disabled:opacity-75 '
-          onClick={() => dispatch(incrementDate())}
-        >
-          &gt;
-        </button>
+        <Button text='<' onClick={() => dispatch(decrementDate())} className='mr-3' />
+        <Button text='>' onClick={() => dispatch(incrementDate())} className='mr-3' />
+        <h4 className='text-center text-lg'>{targetDate.format(yearView ? 'YYYY' : 'MMMM YYYY')}</h4>
       </div>
       <CalViewDropdown />
       <AccountViewDropdown />
