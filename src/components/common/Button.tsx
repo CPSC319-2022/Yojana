ba24a13e0a9c06b40b9470a34d1f438022220458
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-02-17 18:34:37 -0800

    Added collapsable prototype

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,16 +1,20 @@
 import React, { forwardRef } from 'react'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  text: string
+  text?: string
+  padding?: string
 }
 
 export const Button = forwardRef(
-  ({ text, onClick, type, disabled, className }: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
+  (
+    { text = '', onClick, type, disabled, className, padding = 'px-4 py-2' }: ButtonProps,
+    ref: React.Ref<HTMLButtonElement>
+  ) => {
     return (
       <button
         type={type}
         onClick={onClick}
-        className={`inline-flex justify-center rounded-md border border-transparent bg-emerald-100 px-4 py-2 font-medium text-emerald-900 focus:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-2 enabled:hover:bg-emerald-200 disabled:opacity-75 ${className}`}
+        className={`inline-flex justify-center rounded-md border border-transparent bg-emerald-100 font-medium text-emerald-900 focus:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-2 enabled:hover:bg-emerald-200 disabled:opacity-75 ${padding} ${className}`}
         disabled={disabled}
         ref={ref}
       >
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,20 +5,33 @@ import { AppData } from '@/types/AppData'
 import { getCategories } from '@/prisma/queries'
 import { setAppData } from '@/redux/reducers/AppDataReducer'
 import { useAppDispatch } from '@/redux/hooks'
+import { useState } from 'react'
+import { Transition } from '@headlessui/react'
 
 const Calendar = ({ data }: { data: AppData[] }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
   const dispatch = useAppDispatch()
   dispatch(setAppData(data))
 
   return (
     <main>
       <div className='flex h-screen w-full flex-col bg-white text-slate-800'>
-        <NavBar />
+        <NavBar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <div className='border-box flex h-[88vh] w-full flex-row'>
-          <div className='w-1/5 pr-2'>
-            <SideBar />
-          </div>
-          <div className='flex w-4/5 flex-col'>
+          <Transition
+            show={sidebarOpen}
+            enter='transition ease-out duration-200 transform'
+            enterFrom='-translate-x-full'
+            enterTo='translate-x-0'
+            leave='transition ease-in duration-200 transform'
+            leaveFrom='translate-x-0'
+            leaveTo='-translate-x-full'
+          >
+            <div className='pr-2'>
+              <SideBar />
+            </div>
+          </Transition>
+          <div className='flex w-full flex-col'>
             <MainCalendar />
           </div>
         </div>
