8cf253a6760a2fb68877895c84555f6c88d796f6
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-02-28 20:26:54 -0800

    Set DEFAULT_CALLBACK_URL to https://yojana-main.vercel.app in production

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,4 @@
-export const DEFAULT_CALLBACK_URL = 'http://localhost:3000/'
+import * as process from 'process'
+
+export const DEFAULT_CALLBACK_URL =
+  process.env.NODE_ENV === 'production' ? 'https://yojana-main.vercel.app' : 'http://localhost:3000'
