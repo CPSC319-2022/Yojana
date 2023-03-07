9230894a818f688a27d0e1086d7f16d5e4b458bb
Lymeng Naret <lymengnaret@yahoo.com>
2023-03-07 00:22:21 -0800

    added modal and switch for pref

--- src/components/navBar/AccountDropdown.tsx
+++ src/components/navBar/AccountDropdown.tsx
@@ -1,4 +1,5 @@
 import { Dropdown, Modal } from '@/components/common'
+import { Switch } from '@headlessui/react'
 import { signOut, useSession } from 'next-auth/react'
 import { useState } from 'react'
 
@@ -7,6 +8,7 @@ export const AccountViewDropdown = () => {
   const name = session?.user.name || ''
 
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [prefScroll, setPrefScroll] = useState(true)
 
   return (
     <div>
@@ -35,7 +37,7 @@ export const AccountViewDropdown = () => {
       />
       <Modal
         buttonText=''
-        title=''
+        title='Pick Your Preferences'
         isOpen={isModalOpen}
         setIsOpen={setIsModalOpen}
         maxWidth={'40vw'}
@@ -43,11 +45,26 @@ export const AccountViewDropdown = () => {
         closeWhenClickOutside={false}
         handle={'delete-category-modal-handle'}
         bounds={'delete-category-modal-wrapper'}
-        buttonClassName={`group flex w-full items-center rounded-md hover:bg-slate-100 h-0`}
+        buttonClassName={`group flex w-full items-center rounded-md hover:bg-slate-100`}
         showCloseBtn={true}
         overrideDefaultButtonStyle={true}
       >
-        <div className='mt-2 p-3'>hi</div>
+        <div className='space-between mt-2 inline-flex p-3'>
+          <div className='w-[12vw] text-lg'>{prefScroll ? 'Scroll' : 'Expanded'}</div>
+          <Switch
+            checked={prefScroll}
+            onChange={setPrefScroll}
+            className={`${prefScroll ? 'bg-emerald-700' : 'bg-emerald-400'}
+          relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+          >
+            <span className='sr-only'>Use setting</span>
+            <span
+              aria-hidden='true'
+              className={`${prefScroll ? 'translate-x-9' : 'translate-x-0'}
+            pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+            />
+          </Switch>
+        </div>
       </Modal>
     </div>
   )
