d8c086368e01d029d7274b7dbc9efec65ec02a86
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-03-10 15:41:57 -0800

    Added cancel button

--- src/components/CategoryModal/CategoryModal.tsx
+++ src/components/CategoryModal/CategoryModal.tsx
@@ -5,6 +5,7 @@ import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 import { setAlert } from '@/redux/reducers/AlertReducer'
 import { addCategory, getCategory, updateCategory } from '@/redux/reducers/AppDataReducer'
 import {
+  cancelDateSelection,
   getSelectedDates,
   resetSelectedDates,
   setIndividualDates,
@@ -191,6 +192,11 @@ export const CategoryModal = ({ method, id, callBack }: { method: string; id: nu
     callBack()
   }
 
+  const setIsMinimizedCallback = (minimized: boolean) => {
+    setIsMinimized(minimized)
+    dispatch(setIsSelectingDates(minimized))
+  }
+
   return (
     <>
       <Modal
@@ -215,12 +221,22 @@ export const CategoryModal = ({ method, id, callBack }: { method: string; id: nu
         overrideDefaultButtonStyle={method !== 'POST'}
         closeParent={callBack}
         isMinimized={isMinimized}
-        setIsMinimized={(minimized) => {
-          setIsMinimized(minimized)
-          dispatch(setIsSelectingDates(minimized))
-        }}
-        minimizedButtonText='Save Selection'
+        setIsMinimized={setIsMinimizedCallback}
       >
+        <Modal.Minimized>
+          <button
+            type='button'
+            className='mr-3 inline-flex animate-pulse justify-center rounded-md border border-transparent bg-slate-100 py-2 px-4 text-slate-900 enabled:hover:bg-slate-200 disabled:opacity-75'
+            onClick={() => {
+              setIsMinimizedCallback(false)
+              dispatch(cancelDateSelection())
+            }}
+          >
+            Cancel
+          </button>
+          <Button text='Save' onClick={() => setIsMinimizedCallback(false)} className='animate-pulse' />
+        </Modal.Minimized>
+
         <form onSubmit={handleSubmit(onSubmit)} className='mt-2'>
           <div className='mb-4'>
             <label className='mb-2 block'>Name</label>
