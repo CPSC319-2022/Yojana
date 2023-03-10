7367c7f7417c69f811d6ed16a237929cac3aae6c
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-03-10 01:34:46 -0800

    Make switches smaller

--- src/components/common/Toggle.tsx
+++ src/components/common/Toggle.tsx
@@ -32,13 +32,13 @@ export const Toggle = ({
             : dispatch(setGridPreferences(!preference))
         }}
         className={`${preference ? 'bg-emerald-600' : 'bg-emerald-400'}
-        relative inline-flex h-[24px] w-[48px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+        relative inline-flex h-[20px] w-[40px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
       >
         <span className='sr-only'>Use setting</span>
         <span
           aria-hidden='true'
-          className={`${preference ? 'translate-x-6' : 'translate-x-0'}
-          pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+          className={`${preference ? 'translate-x-5' : 'translate-x-0'}
+          pointer-events-none inline-block h-[16px] w-[16px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
         />
       </Switch>
     </div>
