d872f516b5e584695c9a9b73dc496a07db73656f
tsomani <tabreek.somani@gmail.com>
2023-03-13 15:52:48 -0700

    Month View Overflow and 4 month Icon view

--- src/components/mainCalendar/Month.tsx
+++ src/components/mainCalendar/Month.tsx
@@ -1,15 +1,17 @@
-import { useCallback } from 'react'
+import React, { Fragment, useCallback, useEffect, useRef, useState } from 'react'
 import { CategoryBlock } from '@/components/mainCalendar/CategoryBlock'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 import { getCategoryMap, getPrevCurrNextMonth } from '@/redux/reducers/AppDataReducer'
 import { getDate, isMonthInterval, isYearInterval } from '@/redux/reducers/MainCalendarReducer'
 import dayjs from 'dayjs'
+import { Popover, Transition } from '@headlessui/react'
+
 import {
   getIsSelectingDates,
   getPrevCurrNextMonthSelectedDates,
   toggleIndividualDate
 } from '@/redux/reducers/DateSelectorReducer'
-import { IconName } from '@/components/common'
+import { Icon, IconName } from '@/components/common'
 
 interface MonthProps {
   monthOffset: number
@@ -17,6 +19,13 @@ interface MonthProps {
 }
 
 export const Month = (props: MonthProps) => {
+  useEffect(() => {
+    node = myRef.current
+    if (node) {
+      setClientHeight(node.clientHeight - 0.5 * 2)
+      vh = window.innerHeight
+    }
+  }, [])
   const monthView = useAppSelector(isMonthInterval)
   const stateDate = useAppSelector(getDate)
   const referenceDate = useAppSelector(isYearInterval) ? dayjs(stateDate).startOf('year') : stateDate
@@ -26,6 +35,14 @@ export const Month = (props: MonthProps) => {
   const monthStartDate = targetDate.startOf('month')
   const daysInMonth = targetDate.daysInMonth()
   const numWeeks = Math.ceil((daysInMonth + monthStartDate.day()) / 7)
+  const myRef = useRef<HTMLDivElement>(null)
+  let completed = false
+  const [clientHeight, setClientHeight] = useState(0)
+  let node: HTMLDivElement | null
+  let scrollHeight = 0
+  let vh: number
+
+  let extraEvents = 0
 
   const categoryMap = useAppSelector(getCategoryMap)
   const { prevMonth, currMonth, nextMonth } = useAppSelector((state) => getPrevCurrNextMonth(state, targetDate))
@@ -37,9 +54,13 @@ export const Month = (props: MonthProps) => {
 
   const renderDay = useCallback(
     (firstDateOfWeek: number, dayNum: number) => {
+      completed = false
       const offsetFromMonthStart = firstDateOfWeek + dayNum
       const day = monthStartDate.add(offsetFromMonthStart, 'days')
+      let allDayBlocks: (JSX.Element | undefined)[] = []
       let dayBlocks: (JSX.Element | undefined)[] = []
+      let icons: (JSX.Element | undefined)[] = []
+
       let selected: { isSelected: boolean; isRepeating: boolean } = { isSelected: false, isRepeating: false }
       if (!isSelectingDates) {
         let entriesOnDay
@@ -51,7 +72,7 @@ export const Month = (props: MonthProps) => {
           entriesOnDay = currMonth?.[day.date()]
         }
 
-        dayBlocks = entriesOnDay?.map((entry, key) => {
+        allDayBlocks = entriesOnDay?.map((entry, key) => {
           const category = categoryMap[entry.categoryId]
           if (category.show) {
             return (
@@ -59,6 +80,47 @@ export const Month = (props: MonthProps) => {
             )
           }
         })
+        icons = entriesOnDay?.map((calEvent, key) => {
+          const category = categoryMap[calEvent.categoryId]
+          if (category.show) {
+            return (
+              <span className={'px-0.5 font-bold'} key={`${calEvent.id}-${key}`}>
+                <style jsx>{`
+                  * {
+                    color: ${category.color};
+                  }
+                `}</style>
+                <Icon iconName={category.icon as IconName} className='inline' />
+              </span>
+            )
+          }
+        })
+        scrollHeight = 0
+        extraEvents = 0
+
+        dayBlocks = entriesOnDay?.map((entry, key) => {
+          const category = categoryMap[entry.categoryId]
+          if (typeof window !== 'undefined') {
+            vh = window.innerHeight * 0.01
+            scrollHeight += 2 * (vh + 10)
+            if (category.show) {
+              if (scrollHeight + vh + 8 < clientHeight) {
+                scrollHeight += vh + 8
+                return (
+                  <CategoryBlock
+                    color={category.color}
+                    label={category.name}
+                    icon={category.icon as IconName}
+                    key={key}
+                  />
+                )
+              } else {
+                extraEvents++
+              }
+            }
+          }
+        })
+        completed = true
       } else {
         if (offsetFromMonthStart < 0) {
           selected = prevMonthSelected?.[day.date()]
@@ -76,11 +138,10 @@ export const Month = (props: MonthProps) => {
 
       return (
         <div
-          className={`tile overflow-y-auto ${
-            selected?.isSelected ? (selected?.isRepeating ? 'bg-slate-100' : 'bg-emerald-100') : 'bg-white'
-          } ${
-            isSelectingDates && !selected?.isSelected ? 'hover:ring-2 hover:ring-inset hover:ring-emerald-200' : ''
-          } px-0.5 ${isSelectingDates && !selected?.isRepeating ? 'cursor-pointer' : ''} `}
+          ref={myRef}
+          className={`tile overflow-y-hidden ${selected?.isSelected ? 'bg-emerald-100' : 'bg-white'} px-0.5 ${
+            isSelectingDates && !selected?.isRepeating ? 'cursor-pointer' : ''
+          } `}
           key={day.date()}
           onClick={() => {
             if (!selected || !selected?.isRepeating) {
@@ -89,13 +150,45 @@ export const Month = (props: MonthProps) => {
           }}
         >
           <div className={`flex items-center justify-center`}>
-            <div className={`flex h-7 w-7 items-center justify-center ${todayCircle} mt-1`}>
+            <div className={`flex h-3 w-7 items-center justify-center ${todayCircle} mt-1`}>
               <span className={`${notCurrentMonth ? 'text-slate-400' : ''} block text-center text-sm`}>
                 {day.date()}
               </span>
             </div>
           </div>
-          {dayBlocks}
+          {completed && monthView && dayBlocks}
+          {completed && !monthView && icons}
+          {completed && monthView && extraEvents > 0 && (
+            <Popover>
+              <Popover.Button>
+                <button className={'justify-center'}>{extraEvents + ' more'}</button>
+              </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter='transition ease-out duration-200'
+                enterFrom='opacity-0 translate-y-0'
+                enterTo='opacity-100 translate-y--50'
+                leave='transition ease-in duration-150'
+                leaveFrom='opacity-100 translate-y-0'
+                leaveTo='opacity-0 translate-y-1'
+              >
+                <Popover.Panel className=' -translate-y-50 z-100 absolute mt-0 h-full max-h-[400px] w-60 transform overflow-y-auto px-4 sm:px-0 lg:max-w-3xl'>
+                  <div className=' rounded-lg shadow-lg ring-1 ring-black ring-opacity-5'>
+                    <div className='lg:grid-rows relative grid gap-1 bg-white p-2'>
+                      <span
+                        className={`${
+                          offsetFromMonthStart < 0 || offsetFromMonthStart >= daysInMonth ? 'text-slate-400' : ''
+                        } block text-center`}
+                      >
+                        {day.date()}
+                      </span>
+                      {allDayBlocks}
+                    </div>
+                  </div>
+                </Popover.Panel>
+              </Transition>
+            </Popover>
+          )}
         </div>
       )
     },
