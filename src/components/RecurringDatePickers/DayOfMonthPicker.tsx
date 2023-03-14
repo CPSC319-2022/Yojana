7875365d621220cfa97d9d73c095ec313c8e1635
Graywing13 <30674558+Graywing13@users.noreply.github.com>
2023-03-13 22:32:32 -0700

    Add Recurring Monthly Dates
    * Clean up CategoryModal.tsx & errors
    
    - Fix dupe key issue in Year.tsx
    - Make modal exit button be sticky so I don't have to scroll to the top to exit
    - Say "Recurring Dates" instead of "Repeating"
    - Refactor modal to be split up into respective components
    - Wrap function calls in useCallback
    
    * Minor CategoryModal improvements
    
    - Hover turns text to lighter color when user's mouse is on "Recurring Dates"
    - Start date / end date picker switches mouse to pointer when hovered
    
    * Add monthly recurring feature
    
    * Add examples for each recurrence type
    
    * Use buttons, clear if cron switched
    
    - Use buttons instead of dropdown
    - If cron changed from weekly to monthly, clear the field
    
    * clears selected dayofweek when click on monthly
    
    * clear selected DayofWeek when switching to monthly
    
    * save whether weekly monthly Tab, clear between switching
    
    * close edit/delete when click outside (#137)
    
    * add a div to closeWhenClickOutside of dropdown
    
    * minimize dropdown when modal is active
    
    * Center button text, fix monthly NONE
    
    - Make month text not always default to NONE by removing cron parsing conflict
    - Center button text vertically
    
    * Fix weekly crons being resetted when monthly tab clicked
    
    - was supposed to use a useCallback bc we only wanted it to change when a button was clicked, instead of on any update
    
    ---------
    
    Co-authored-by: Lymeng Naret <lymengnaret@yahoo.com>
    Co-authored-by: Lymeng <60766732+NLmeng@users.noreply.github.com>

---
+++ src/components/RecurringDatePickers/DayOfMonthPicker.tsx
@@ -0,0 +1,138 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import { useController } from 'react-hook-form'
+import { Dayjs } from 'dayjs'
+import { DropdownMenuItem } from '@/components/common/Dropdown'
+
+interface DayOfMonthPickerProps {
+  control: any
+  name: string
+  rules?: any
+  startDate: Dayjs
+  selectedRecurrenceType: MonthRecurrenceType
+  setSelectedRecurrenceType: React.Dispatch<React.SetStateAction<MonthRecurrenceType>>
+  updateState?: (cron: string) => void
+}
+
+export enum MonthRecurrence {
+  NONE = 'NONE',
+  ON_DATE_Y = 'ON_DATE_Y', // E.g. "Monthly on day 1"
+  ON_LAST_DAY = 'ON_LAST_DAY', // E.g. "Monthly on the last day"
+  ON_YTH_XDAY = 'ON_YTH_XDAY', // E.g. "Monthly on the fourth Sunday"
+  ON_LAST_XDAY = 'ON_LAST_XDAY' // E.g. "Monthly on the last Sunday"
+}
+export type MonthRecurrenceType = keyof typeof MonthRecurrence
+
+// Note: weekNum is defined as ceil(day.date() / 7). So the 7th is always weekNum = 1.
+const weekNumReplacer = '{weekNum}'
+const dayOfWeekReplacer = '{dayOfWeek}'
+const dateOfMonthReplacer = '{dateOfMonth}'
+export const monthRecurrenceCrons: { [key in MonthRecurrenceType]: string } = {
+  NONE: '',
+  ON_DATE_Y: `0 0 ${dateOfMonthReplacer} * *`,
+  ON_LAST_DAY: `0 0 L * ?`,
+  ON_YTH_XDAY: `0 0 ? * ${dayOfWeekReplacer}#${weekNumReplacer}`,
+  ON_LAST_XDAY: `0 0 ? * ${dayOfWeekReplacer}L`
+}
+
+const ordinals = ['first', 'second', 'third', 'fourth']
+
+export const DayOfMonthPicker = ({
+  control,
+  name,
+  rules,
+  startDate,
+  selectedRecurrenceType,
+  setSelectedRecurrenceType,
+  updateState = () => {}
+}: DayOfMonthPickerProps) => {
+  const {
+    field: { onChange }
+  } = useController({
+    name: name,
+    control: control,
+    rules: rules
+  })
+
+  const [dateOfMonth, setDateOfMonth] = useState(startDate.date())
+  const [weekNum, setWeekNum] = useState(Math.ceil(startDate.date() / 7))
+  const [dayOfWeek, setDayOfWeek] = useState(startDate.day())
+  const [recurrenceType, setRecurrenceType] = useState(selectedRecurrenceType)
+
+  useEffect(() => {
+    setDateOfMonth(startDate.date())
+    setWeekNum(Math.ceil(startDate.date() / 7))
+    setDayOfWeek(startDate.day())
+  }, [startDate])
+
+  const handleRecurrenceChange = useCallback(
+    (newType: MonthRecurrenceType) => {
+      const cronString = monthRecurrenceCrons[newType]
+        .replace(weekNumReplacer, weekNum.toString())
+        .replace(dayOfWeekReplacer, dayOfWeek.toString())
+        .replace(dateOfMonthReplacer, dateOfMonth.toString())
+
+      onChange(cronString)
+      updateState(cronString)
+      setRecurrenceType(newType)
+      setSelectedRecurrenceType(newType)
+    },
+    [dateOfMonth, dayOfWeek, onChange, setSelectedRecurrenceType, updateState, weekNum]
+  )
+
+  const availableMenuItems: { [key in MonthRecurrenceType]: DropdownMenuItem } = useMemo(() => {
+    return {
+      NONE: { key: MonthRecurrence.NONE, label: 'None', onClick: () => handleRecurrenceChange(MonthRecurrence.NONE) },
+      ON_DATE_Y: {
+        key: MonthRecurrence.ON_DATE_Y,
+        label: `Monthly on day ${dateOfMonth}`,
+        onClick: () => handleRecurrenceChange(MonthRecurrence.ON_DATE_Y)
+      },
+      ON_LAST_DAY: {
+        key: MonthRecurrence.ON_LAST_DAY,
+        label: `Monthly on the last day`,
+        onClick: () => handleRecurrenceChange(MonthRecurrence.ON_LAST_DAY)
+      },
+      ON_YTH_XDAY: {
+        key: MonthRecurrence.ON_YTH_XDAY,
+        label: `Monthly on the ${ordinals[weekNum - 1]} ${startDate.format('dddd')}`,
+        onClick: () => handleRecurrenceChange(MonthRecurrence.ON_YTH_XDAY)
+      },
+      ON_LAST_XDAY: {
+        key: MonthRecurrence.ON_LAST_XDAY,
+        label: `Monthly on the last ${startDate.format('dddd')}`,
+        onClick: () => handleRecurrenceChange(MonthRecurrence.ON_LAST_XDAY)
+      }
+    }
+  }, [dateOfMonth, handleRecurrenceChange, startDate, weekNum])
+
+  const getMenuItems = useMemo(() => {
+    const includeItems = [MonthRecurrence.NONE, MonthRecurrence.ON_DATE_Y]
+
+    if (dateOfMonth === startDate.daysInMonth()) includeItems.push(MonthRecurrence.ON_LAST_DAY)
+    if (weekNum <= 4) includeItems.push(MonthRecurrence.ON_YTH_XDAY)
+    if (dateOfMonth > startDate.daysInMonth() - 7) includeItems.push(MonthRecurrence.ON_LAST_XDAY)
+
+    if (!includeItems.includes(MonthRecurrence[recurrenceType])) setRecurrenceType(MonthRecurrence.NONE)
+
+    return includeItems.map((item) => availableMenuItems[item])
+  }, [availableMenuItems, dateOfMonth, recurrenceType, startDate, weekNum])
+
+  return (
+    <div className='flex grid grid-cols-2 flex-wrap justify-center gap-1 pt-3'>
+      {getMenuItems.map((item) => {
+        const isActive = recurrenceType === item.key
+        return (
+          <button
+            key={item.key}
+            type='button'
+            onClick={item.onClick}
+            className={`group flex w-full rounded-md px-2 py-2 text-left
+            ${isActive ? 'bg-emerald-100 hover:bg-emerald-200' : 'bg-slate-100 hover:bg-slate-200'}`}
+          >
+            {item.label}
+          </button>
+        )
+      })}
+    </div>
+  )
+}
--- src/components/mainCalendar/Year.tsx
+++ src/components/mainCalendar/Year.tsx
@@ -21,7 +21,7 @@ export const Year = () => {
   const dispatch = useAppDispatch()
 
   const renderDayCategories = useCallback(
-    (day: Dayjs, monthNum: number) => {
+    (day: Dayjs, monthNum: number, key: number) => {
       let icons: (JSX.Element | undefined)[] = []
 
       if (!isSelectingDates) {
@@ -42,7 +42,7 @@ export const Year = () => {
           }
         })
       }
-      icons.push(<span key={`${monthNum}-${day.get('day')}`}>&nbsp;</span>)
+      icons.push(<span key={`${monthNum}-${key}`}>&nbsp;</span>)
       return icons
     },
     [categoryMap, entriesInYear, isSelectingDates]
@@ -98,7 +98,7 @@ export const Year = () => {
           key={`${yearNum}-${monthNum}-${dateOffset}`}
           onClick={() => onDayClicked(day, !selected || !selected?.isRepeating)}
         >
-          {renderDayCategories(day, monthNum)}
+          {renderDayCategories(day, monthNum, dateOffset)}
         </div>
       )
     },
