ca551c3f0c1b512fc5cdae32f135144eaf0df972
Graywing13 <30674558+Graywing13@users.noreply.github.com>
2023-03-16 16:22:06 -0700

    Added Quarterly View (#164)
    * Refactor Month.tsx in prep for quarterly
    
    * Refactor further, add colour changes
    
    - Only render headers if month view (to save space)
    - To make up for it, make weekends bg-slate-100
    
    * Add Quarterly, general month changes
    
    - Add quarterly view
    - Gray out out-of-month dates
    - Revert month view colour change
    - As an added benefit, 4-months now looks less scuffed.
    
    * Fix banners
    
    - Make banners show text and icon instead of only the line
    - Remove icon opacity because it doesn't work with z-index
    
    * Fix 6-week months missing horizontal grid lines
    
    * Fix window infinite resizing issue
    
    * Add weekend highlights back to month view
    
    * Month name & spacing improvements
    
    - Make month name font medium instead of semibold
    - Make month name box be a calculated width, instead of fixed at 32px (for medium screens)
    - Add truncate to import button so that it shrinks at the same rate as the create button
    
    * Month name & spacing improvements
    
    - Make month name font medium instead of semibold
    - Make month name box be a calculated width, instead of fixed at 32px (for medium screens)
    - Add truncate to import button so that it shrinks at the same rate as the create button
    
    * Rename Master Categories to Master Calendar
    
    closes #159
    
    * Refactor Month.tsx in prep for quarterly
    
    * Rebase on main
    
    * Rebase part 2
    
    * Fix banners
    
    - Make banners show text and icon instead of only the line
    - Remove icon opacity because it doesn't work with z-index
    
    * Fix 6-week months missing horizontal grid lines
    
    * Fix window infinite resizing issue
    
    * Add weekend highlights back to month view
    
    * Month name & spacing improvements
    
    - Make month name font medium instead of semibold
    - Make month name box be a calculated width, instead of fixed at 32px (for medium screens)
    - Add truncate to import button so that it shrinks at the same rate as the create button
    
    * Month name & spacing improvements
    
    - Make month name font medium instead of semibold
    - Make month name box be a calculated width, instead of fixed at 32px (for medium screens)
    - Add truncate to import button so that it shrinks at the same rate as the create button
    
    * Rebase on main part 3
    
    ---------
    
    Co-authored-by: AnimeAllstar <asadrehandhorajiwala@gmail.com>

--- src/components/mainCalendar/CategoryBlock.tsx
+++ src/components/mainCalendar/CategoryBlock.tsx
@@ -1,5 +1,3 @@
-import { useAppSelector } from '@/redux/hooks'
-import { isMonthInterval } from '@/redux/reducers/MainCalendarReducer'
 import { getTextColor } from '@/utils/color'
 import React from 'react'
 import { Icon, IconName } from '@/components/common'
@@ -8,11 +6,10 @@ interface CategoryBlockProps {
   color: string
   label: string
   icon: IconName
+  className?: string
 }
 
-export const CategoryBlock = ({ color, label, icon }: CategoryBlockProps) => {
-  const monthView = useAppSelector(isMonthInterval)
-
+export const CategoryBlock = ({ color, label, icon, className }: CategoryBlockProps) => {
   return (
     <>
       <style jsx>{`
@@ -22,17 +19,12 @@ export const CategoryBlock = ({ color, label, icon }: CategoryBlockProps) => {
       `}</style>
       <div
         aria-label={label}
-        className={
-          'event-block mx-1 mt-1 min-h-[1vh] overflow-x-hidden whitespace-nowrap rounded-md px-1.5 ' +
-          getTextColor(color)
-        }
+        className={`event-block mx-1 mt-1 overflow-x-hidden whitespace-nowrap rounded-md px-1.5 
+          ${getTextColor(color)} 
+          ${className}`}
       >
-        {monthView && (
-          <>
-            <Icon iconName={icon} className='mb-0.5 mr-1 inline' />
-            {label}
-          </>
-        )}
+        <Icon iconName={icon} className='mb-0.5 mr-1 inline' />
+        {label}
       </div>
     </>
   )
