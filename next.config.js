a511ab78a5f81a1c03a3fdcfb65ec530719042bc
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-03-22 23:18:07 -0700

    Cache static json files for 6 months

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Set cache-control header for JSON files
+        source: '/(.*)\\.json',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=15552000' // 6 months
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
