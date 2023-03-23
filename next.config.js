858f00a727199fb5df7721687fe745d08fd7ba59
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-03-22 18:53:57 -0700

    Added build analyze command

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ const nextConfig = {
   reactStrictMode: true
 }
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+module.exports = withBundleAnalyzer(nextConfig)
