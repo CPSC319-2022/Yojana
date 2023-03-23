788f76a47b430088d0cf95c7ecba120e8c30aafc
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-03-22 22:35:59 -0700

    Revert removing module.exports = nextConfig

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true
 }
 
+module.exports = nextConfig
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
