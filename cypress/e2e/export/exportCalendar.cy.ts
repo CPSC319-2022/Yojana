022ccecef486510f0ee7997ceebfbb9c2322d80b
daniellebuggle <99493192+daniellebuggle@users.noreply.github.com>
2023-03-27 23:35:47 -0700

    extracted duplicate methods

--- cypress/e2e/export/exportCalendar.cy.ts
+++ cypress/e2e/export/exportCalendar.cy.ts
@@ -8,6 +8,19 @@ describe('Export Calendars', () => {
     cy.get(`div#category-item-${number}`).find('input[type="checkbox"]').should('have.prop', 'checked', false)
   }
   const adminID = 'f0b54ab0-366f-45b1-b750-1d5b79f3603c'
+  const plebID = 'e7da4fbe-9a15-4935-a369-963ee558f0ea'
+
+  function masterCalendar() {
+    it('should download Master Calendar', () => {
+      cy.get('#account-dropdown').click() // Click the account dropdown
+      cy.contains('Export Calendar').click() // Click the Export Calendar accordion
+      cy.contains('Master Calendar').click() // Click the Master Calendar button
+      cy.request('/api/dates/export?master=true').then((response) => {
+        // Check that the file was downloaded successfully
+        expect(response.status).to.eq(200) // Verify that the HTTP status code is 200 (OK)
+      })
+    })
+  }
 
   describe('admin', () => {
     beforeEach(() => {
@@ -19,15 +32,7 @@ describe('Export Calendars', () => {
       cy.resetDb()
     })
 
-    it('should download Master Calendar', () => {
-      cy.get('#account-dropdown').click() // Click the account dropdown
-      cy.contains('Export Calendar').click() // Click the Export Calendar accordion
-      cy.contains('Master Calendar').click() // Click the Master Calendar button
-      cy.request('/api/dates/export?master=true').then((response) => {
-        // Check that the file was downloaded successfully
-        expect(response.status).to.eq(200) // Verify that the HTTP status code is 200 (OK)
-      })
-    })
+    masterCalendar()
 
     it('should download Personal Calendar', () => {
       cy.get('#account-dropdown').click() // Click the account dropdown
@@ -39,6 +44,10 @@ describe('Export Calendars', () => {
       })
     })
 
+    filteredCategories()
+  })
+
+  function filteredCategories() {
     it('should download Filtered Calendar', () => {
       cy.get('div#sidebar').scrollTo('bottom')
       cy.get(`div#category-item-19`).find(`input[type="checkbox"]`).uncheck()
@@ -46,7 +55,6 @@ describe('Export Calendars', () => {
       for (let i = 1; i < 18; i++) {
         checked(i)
       }
-
       for (let i = 18; i < 20; i++) {
         notChecked(i)
       }
@@ -59,6 +67,30 @@ describe('Export Calendars', () => {
         expect(response.status).to.eq(200) // Verify that the HTTP status code is 200 (OK)
       })
     })
+  }
+
+  describe('pleb', () => {
+    beforeEach(() => {
+      cy.login('pleb')
+      cy.visit('/')
+    })
+
+    afterEach(() => {
+      cy.resetDb()
+    })
+    masterCalendar()
+
+    it('should download Personal Calendar', () => {
+      cy.get('#account-dropdown').click() // Click the account dropdown
+      cy.contains('Export Calendar').click() // Click the Export Calendar accordion
+      cy.contains('Personal Calendar').click() // Click the Personal Calendar button
+      cy.request(`/api/dates/export?master=false&userID=${plebID}`).then((response) => {
+        // Check that the file was downloaded successfully
+        expect(response.status).to.eq(200) // Verify that the HTTP status code is 200 (OK)
+      })
+    })
+
+    filteredCategories()
   })
 })
 
