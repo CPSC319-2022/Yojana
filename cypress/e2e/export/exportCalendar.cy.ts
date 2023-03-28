deb8051592556627c01da09b31260122b71d2f49
daniellebuggle <99493192+daniellebuggle@users.noreply.github.com>
2023-03-27 22:36:37 -0700

    added test for filtered categories

--- cypress/e2e/export/exportCalendar.cy.ts
+++ cypress/e2e/export/exportCalendar.cy.ts
@@ -1,5 +1,14 @@
 describe('Export Calendars', () => {
+  const checkedCats: number[] = []
+  const checked = (number: number) => {
+    cy.get(`div#category-item-${number}`).find('input[type="checkbox"]').should('have.prop', 'checked', true)
+    checkedCats.push(number)
+  }
+  const notChecked = (number: number) => {
+    cy.get(`div#category-item-${number}`).find('input[type="checkbox"]').should('have.prop', 'checked', false)
+  }
   const adminID = 'f0b54ab0-366f-45b1-b750-1d5b79f3603c'
+
   describe('admin', () => {
     beforeEach(() => {
       cy.login('admin')
@@ -25,6 +34,27 @@ describe('Export Calendars', () => {
       cy.contains('Export Calendar').click() // Click the Export Calendar accordion
       cy.contains('Personal Calendar').click() // Click the Personal Calendar button
       cy.request(`/api/dates/export?master=false&userID=${adminID}`).then((response) => {
+        // Check that the file was downloaded successfully
+        expect(response.status).to.eq(200) // Verify that the HTTP status code is 200 (OK)
+      })
+    })
+
+    it('should download Filtered Calendar', () => {
+      cy.get('div#sidebar').scrollTo('bottom')
+      cy.get(`div#category-item-19`).find(`input[type="checkbox"]`).uncheck()
+      cy.get(`div#category-item-18`).find(`input[type="checkbox"]`).uncheck()
+      for (let i = 1; i < 18; i++) {
+        checked(i)
+      }
+
+      for (let i = 18; i < 20; i++) {
+        notChecked(i)
+      }
+      const checkedCatsString = checkedCats.join(',')
+      cy.get('#account-dropdown').click() // Click the account dropdown
+      cy.contains('Export Calendar').click() // Click the Export Calendar accordion
+      cy.contains('Filtered Categories').click() // Click the Personal Calendar button
+      cy.request(`/api/dates/export?categories=${checkedCatsString}`).then((response) => {
         // Check that the file was downloaded successfully
         expect(response.status).to.eq(200) // Verify that the HTTP status code is 200 (OK)
       })
