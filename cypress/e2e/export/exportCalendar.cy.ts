e9c7f9776e9a3b2640c74aea11e67b3e5c12d1f0
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2023-03-29 03:22:48 -0700

    Added tests to check text in ical files

--- cypress/e2e/export/exportCalendar.cy.ts
+++ cypress/e2e/export/exportCalendar.cy.ts
@@ -18,29 +18,43 @@ describe('Export Calendars', () => {
       cy.request('/api/dates/export?master=true').then((response) => {
         // Check that the file was downloaded successfully
         expect(response.status).to.eq(200) // Verify that the HTTP status code is 200 (OK)
+        // Check that the calendar contains dates for the first category (Statutory Holiday)
+        expect(response.body).to.contain('Statutory Holiday')
       })
     })
   }
 
-  function checkFilteredCategories() {
-    const checkedCatsString = checkedCats.join(',')
-    cy.get('#account-dropdown').click() // Click the account dropdown
-    cy.contains('Export Calendar').click() // Click the Export Calendar accordion
-    cy.contains('Filtered Categories').click() // Click the Personal Calendar button
-    cy.request(`/api/dates/export?categories=${checkedCatsString}`).then((response) => {
-      // Check that the file was downloaded successfully
-      expect(response.status).to.eq(200) // Verify that the HTTP status code is 200 (OK)
-    })
-  }
+  before(() => {
+    // Add some dates to the master calendar
+    cy.login('admin')
+    cy.visit('/')
+    // add dates to a master calendar category
+    // Click edit
+    cy.get('button#category-dropdown-1').should('exist').click()
+    cy.get('button#edit-category-btn').should('exist').click()
+
+    // check if modal is open
+    cy.get('div#edit-category-modal-div').should('be.visible')
+
+    // select WED
+    cy.get('div#edit-category-modal-div').scrollTo('bottom')
+    cy.get('button#recurring-dates-disclosure-btn').click()
+    cy.get('button#recurring-dates-tab-weekly').click()
+    cy.get('div#edit-category-modal-div').scrollTo('bottom')
+    cy.get('button#WED-in-weekly').click()
+
+    // Save the category
+    cy.get('button#create-category-submit-btn').trigger('click').click()
+  })
+
+  after(() => {
+    cy.resetDb()
+  })
 
   describe('admin', () => {
     beforeEach(() => {
       cy.login('admin')
       cy.visit('/')
-    })
-
-    afterEach(() => {
-      cy.resetDb()
     })
 
     masterCalendar()
@@ -52,20 +66,30 @@ describe('Export Calendars', () => {
       cy.request(`/api/dates/export?master=false&userID=${adminID}`).then((response) => {
         // Check that the file was downloaded successfully
         expect(response.status).to.eq(200) // Verify that the HTTP status code is 200 (OK)
+        expect(response.body).to.include('PayDay')
       })
     })
 
     it('should download Filtered Calendar', () => {
       cy.get('div#sidebar').scrollTo('bottom')
-      cy.get(`div#category-item-19`).find(`input[type="checkbox"]`).uncheck()
-      cy.get(`div#category-item-18`).find(`input[type="checkbox"]`).uncheck()
-      for (let i = 1; i < 18; i++) {
-        checked(i)
-      }
-      for (let i = 18; i < 20; i++) {
+      for (let i = 14; i < 19; i++) {
+        cy.get(`div#category-item-${i}`).find(`input[type="checkbox"]`).uncheck({ force: true })
         notChecked(i)
       }
-      checkFilteredCategories()
+      cy.get(`div#category-item-19`).find(`input[type="checkbox"]`).check({ force: true })
+      checked(19)
+      const checkedCatsString = checkedCats.join(',')
+      cy.get('#account-dropdown').click() // Click the account dropdown
+      cy.contains('Export Calendar').click() // Click the Export Calendar accordion
+      cy.contains('Filtered Categories').click() // Click the Personal Calendar button
+      cy.request(`/api/dates/export?categories=${checkedCatsString}`).then((response) => {
+        // Check that the file was downloaded successfully
+        expect(response.status).to.eq(200) // Verify that the HTTP status code is 200 (OK)
+        // check that category (14 - PayDay) is not included
+        expect(response.body).to.not.include('PayDay')
+        // check that category (19 - Birthday) is included
+        expect(response.body).to.include('Birthday')
+      })
     })
   })
 
@@ -73,10 +97,6 @@ describe('Export Calendars', () => {
     beforeEach(() => {
       cy.login('pleb')
       cy.visit('/')
-    })
-
-    afterEach(() => {
-      cy.resetDb()
     })
 
     masterCalendar()
@@ -89,19 +109,6 @@ describe('Export Calendars', () => {
         // Check that the file was downloaded successfully
         expect(response.status).to.eq(200) // Verify that the HTTP status code is 200 (OK)
       })
-    })
-
-    it('should download Filtered Calendar', () => {
-      cy.get('div#sidebar').scrollTo('bottom')
-      cy.get(`div#category-item-13`).find(`input[type="checkbox"]`).uncheck()
-      cy.get(`div#category-item-12`).find(`input[type="checkbox"]`).uncheck()
-      for (let i = 1; i < 12; i++) {
-        checked(i)
-      }
-      for (let i = 12; i < 14; i++) {
-        notChecked(i)
-      }
-      checkFilteredCategories()
     })
   })
 })
