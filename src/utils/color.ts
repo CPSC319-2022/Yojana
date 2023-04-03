58279257e29d39d87e9ebc41078e08ed666971d4
dashdot <30674558+Graywing13@users.noreply.github.com>
2023-04-02 20:54:50 -0700

    Add documentation for globals.css, utils, and redux

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,14 +1,26 @@
+/**
+ * Reusable helper functions related to colours in the app
+ */
+
 import { hex } from 'wcag-contrast'
 
-// returns text-white or text-slate-800 depending on the contrast ratio
-// a contrast ratio of 4.5 is the minimum for WCAG AA compliance
-// by checking if bgColor contrast ratio with white is greater than 4.5 we can determine if the text should be white or black
+/**
+ * Used for determining text colour, given the background colour of the text.
+ *
+ * Returns text-white or text-slate-800 depending on the contrast ratio.
+ * a contrast ratio of 4.5 is the minimum for WCAG AA compliance
+ * by checking if bgColor contrast ratio with white is greater than 4.5 we can determine if the text should be white or black
+ *
+ * @param bgColor - the background colour of the target text
+ */
 export const getTextColor = (bgColor: string) => {
   return hex(bgColor, '#ffffff') > 4.5 ? 'text-white' : 'text-slate-800'
 }
 
-// generate a random hex color
-// https://stackoverflow.com/a/5092846/8488681
+/**
+ * Generate a random hex color
+ * https://stackoverflow.com/a/5092846/8488681
+ */
 export const randomColor = () => {
   return '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0')
 }
--- src/utils/preprocessEntries.ts
+++ src/utils/preprocessEntries.ts
@@ -1,3 +1,7 @@
+/**
+ * Configuration and helper functions used to preprocessing dates to be in the right format for the webapp.
+ */
+
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
@@ -15,10 +19,21 @@ dayjs.tz.setDefault('America/Vancouver')
 // Set the process timezone to UTC
 process.env.TZ = 'UTC'
 
+/**
+ * Return a JavaScript native date, without any associated time
+ *
+ * @param date - JS date object that may have an associated time
+ */
 export const getLocalDateWithoutTime = (date: Date) => {
   return dayjs.utc(date).tz().startOf('day').toDate()
 }
 
+/**
+ * Given a list of entries corresponding to various categories' occurrences,
+ * remove their dates' associated timezones.
+ *
+ * @param entries - a list of entries, where each represents a single instance of a category's occurence
+ */
 export const preprocessEntries = (entries: Entry[]) => {
   return entries.map((entry) => {
     return {
